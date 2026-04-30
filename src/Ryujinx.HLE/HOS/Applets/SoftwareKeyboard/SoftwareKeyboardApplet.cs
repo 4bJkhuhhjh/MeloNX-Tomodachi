7cd3ce4721a9b1a17dcb701e8c09e91d79474c4d
4bJkhuhhjh <138670915+4bJkhuhhjh@users.noreply.github.com>
2026-04-30 07:27:20 +0700

    Update SoftwareKeyboardApplet.cs

--- src/Ryujinx.HLE/HOS/Applets/SoftwareKeyboard/SoftwareKeyboardApplet.cs
+++ src/Ryujinx.HLE/HOS/Applets/SoftwareKeyboard/SoftwareKeyboardApplet.cs
@@ -14,7 +14,7 @@ using System.IO;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading;
+using System.Threading.Tasks;
 
 namespace Ryujinx.HLE.HOS.Applets
 {
